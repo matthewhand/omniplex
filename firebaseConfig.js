17cd122d1ca6ee0ad52f9dc5838f56d367e6b881
Matthew Hand <matthewhandau@gmail.com>
2024-10-16 14:16:30 +1100

    Update .env.example with new environment variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
